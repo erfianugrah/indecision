f6c45f82e2057b082424ab9989884492033ee178
Erfi Anugrah <erfi.anugrah@gmail.com>
2022-09-21 11:53:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# React-based RNG App
---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,8 @@
+// Block Scoping
+
+const fullName = 'Erfi Anugrah';
+
+if (fullName) {
+    const  firstName = fullName.split(' ')[0];
+    console.log(firstName);
+}
